09924545783569a4fe2d51cfb9662bc84ff4a53f
darklegend99 <darklegend1931@gmail.com>
2021-07-18 14:15:48 +0530

    Complete currencyConverter project
    This project can now convert any given quantity of crypto currencies(BTC,ETH,LTC) into fait currencies(USD,EUR,GBP).

--- Currency Converter/index.js
+++ Currency Converter/index.js
@@ -9,33 +9,42 @@ app.get("/", function(req,res){
     res.sendFile(__dirname+"/index.html");
 });
 
-// var options = {
-//   method: 'GET',
-//   url: 'https://currencyapi-net.p.rapidapi.com/convert',
-//   params: {from: 'BTC', to: 'USD', amount: '1', output: 'JSON'},
-//   headers: {'x-rapidapi-host': 'currencyapi-net.p.rapidapi.com',
-//   "x-rapidapi-key": ""}
-// };
 
+// var crypto = require('crypto-js');
 
-
-
-app.post("/", function(req,res){    
+app.post("/", function(req,res){  
 
     var crypto = req.body.crypto;
     var fiat = req.body.fiat;
-    var baseUrl = "https://apiv2.bitcoinaverage.com/indices/global/ticker/";
-    var finalUrl = baseUrl+crypto+fiat;
-    request(finalUrl, function(error,response,body){
-        // console.log(body);
+    var amount = req.body.amount;
+    
+    var public_key = '';
+    // var ticker_btcusd_url = 'https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD';
+    var ticker_url = 'https://apiv2.bitcoinaverage.com/convert/global';
+
+    var options = {
+    url: ticker_url,
+    method: "GET",
+    // params: {from: crypto, to: fiat, amount: 1, output: 'JSON'},
+    qs: {from: crypto, to: fiat, amount: amount}, 
+        headers: {
+            "Content-Type": "application/json",
+            "x-ba-key": public_key,
+        }
+    };
+
+    
+    request(options, function(error, response, body) {
+        console.log(body);
+        // console.log(typeof(body));
+        
         var data = JSON.parse(body);
-        // console.log(data);
-        var price = data.last; 
+        var price = data.price;
         console.log(price);
 
-        var currentDate = data.display_timestamp;
+        var currentDate = data.time;
         res.write("<p>The current date is: "+ currentDate + "</p>");
-        res.write("<h1>The current price of " + crypto + " is " + price + fiat+ " USD</h1>");
+        res.write("<h1>The current price of " + amount + crypto + " is " + price + fiat+ " USD</h1>");
 
         res.send();
     });
@@ -43,4 +52,35 @@ app.post("/", function(req,res){
 
 app.listen(3000, function(){
     console.log("Server is listening at port 3000");
-})
+});
+
+
+
+
+
+
+
+
+//     try{
+//     var crypto = req.body.crypto;
+//     var fiat = req.body.fiat;
+//     var baseURL="https://apiv2.bitcoinaverage.com/indices/global/ticker/";
+//     var finalUrl = baseUrl+crypto+fiat;
+//     request(finalUrl, function(error,response,body){
+//         // console.log(body);
+//         var data = JSON.parse(body);
+//         // console.log(data);
+//         var price = data.last; 
+//         console.log(price);
+
+//         var currentDate = data.display_timestamp;
+//         res.write("<p>The current date is: "+ currentDate + "</p>");
+//         res.write("<h1>The current price of " + crypto + " is " + price + fiat+ " USD</h1>");
+
+//         res.send();
+//     });
+// }
+// catch(err){
+//     console.log(err);
+// }
+// });
